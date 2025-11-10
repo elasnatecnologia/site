b3cdd31eaba5faef03b754852af533968228df8c
tuiza <luiza8.marlene@gmail.com>
2025-11-10 20:45:42 -0300

    build: change page

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-  base: "/elas_na_tecnologia/",
+  base: "/site/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
